825a773316c46b3f937e47847e9bd8c5c81b224a
Leomzito <redllamarockz9000@gmail.com>
2026-09-15 17:04:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     <form method="POST">
         <input type = "text" id="nome" name="nome" placeholder="Digite seu nome">
         <input type = "number" id="idade" name="idade" placeholder="Digite sua idade">
-        <button type = "button"> Enviar </button>
+        <button type "button" value="Enviar">
     </form>
     <div class = "container">
         
