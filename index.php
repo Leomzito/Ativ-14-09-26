ba9c33b7ceeae9e20ad4683e91a33c520fd4f654
Leomzito <redllamarockz9000@gmail.com>
2026-09-21 15:02:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,12 @@
-<?php
-    $nome = "";
-    $idade = 0;
-    $resposta= "";
-    if ($_SERVER ["REQUEST_METHOD"]=="POST"){
-        $nome = $_POST["nome"];
-        $idade = $_POST["idade"];
-         if ($idade >= 18){
-            $resposta="Você é maior de idade";
-        }
-        else{
-            $resposta="Você é menor de idade";    
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <form method="POST">
-        <input type = "text" id="nome" name="nome" placeholder="Digite seu nome">
-        <input type = "number" id="idade" name="idade" placeholder="Digite sua idade">
-        <button type="submit">Verificar</button>
-    </form>
-    <div class = "container">
-        
-
-        <?php  if($resposta != "") { ?>
-            <h2>O nome é: <?= $nome ?> </h2>
-            <h2>A idade é: <?= $idade ?> anos </h2>
-            <h2> <?= $resposta ?> </h2>
-
-        <?php } ?>
-    </div>
+    <a href="idade.php">Verificador de idade</a>
+    <a href="notas.php">Verificador de notas</a>
 </body>
 </html>
